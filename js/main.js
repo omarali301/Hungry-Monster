6f9e650b56b080df87532391b05b20f32be9105a
unknown <you@example.com>
2021-02-08 14:43:22 +0600

    all add

--- js/main.js
+++ js/main.js
@@ -1,80 +1,83 @@
-
-
-//list item area----------------------------------------
-
-const searchBtn = document.getElementById("submit");
-searchBtn.addEventListener("click", () =>{
-    const inputName = document.getElementById('place').value;
-
-    getItemName(inputName);
-
-    // console.log(inputName);
-
-})
-
-
-    function getItemName(name){
-
-        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
-
-        fetch(url)
-
-        .then(response => response.json())
-        .then(data => displayFood(data.meals));
-
-
-    }
-
-    function displayFood(foods){
-        
-
-        const foodDiv = document.getElementById("show-item")
-
-        foods.forEach(food => {
-
-            const div = document.createElement("div");
-
-            div.className = 'div-style'
-            foodDiv.className = 'foodDiv-style'
-
-
-            const foodInfo =`
-
-            <img onclick ="getItemDetails('${food.strMeal}')" src ="${food.strMealThumb}">
-
-            <h3>${food.strMeal}</h3>
-
-           `
-            div.innerHTML = foodInfo;
-
-foodDiv.appendChild(div);
-
-
-
-
-            
-        });
-
-
-
-        
-
-    }
-
-//             //details area-------------------------------------------------
-
-
-
-
-
-function getItemDetails(name){
-
-    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${name}`;
-
+const getMealInput = () => {
+    const mealInput = document.getElementById("input-text").value;
+    const ingredients = document.getElementById("meal-list");
+  
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealInput}`;
+  
     fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+          
+        const foodsDiv = document.getElementById("meal-list");
+        let mealsInput = data.meals;
+        let foodList = "";
+        mealsInput.forEach((element) => {
 
-    .then(response => response.json())
-    .then(data => console.log(data.name));
+            foodList += ` 
 
 
-}
+            <div onClick="getMealItem('${element.strMeal}')" class="food">
+              <img class="card-img-top" src="${element.strMealThumb}" />
+              <div class="card-body">
+                <h5 class="card-title text-center">${element.strMeal}</h5>
+              </div>
+            </div>
+            `;
+
+
+        });
+        foodsDiv.innerHTML = foodList;
+      })
+      .catch(function () {
+        const foodError = document.getElementById("meal-list");
+        let foodErrorString = "";
+        foodErrorString = `<h2 class="meal-error">Sorry!We don't find any meal..</h2>`;
+        foodError.innerHTML = foodErrorString;
+      });
+  
+  };
+
+
+  
+  const getMealItem = (mealsIngredients) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealsIngredients}`;
+  
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        const meal = data.meals;
+        ingredientsFood(meal[0]);
+      });
+  };
+  
+  
+
+
+
+
+  const ingredientsFood = (mealsIngredients) => {
+    const MealDetailsDiv = document.getElementById("meal-list");
+    MealDetailsDiv.style.display = "block";
+    MealDetailsDiv.innerHTML = `
+    <div class="meal-list">
+       <img src="${mealsIngredients.strMealThumb}"/>
+          <h3>${mealsIngredients.strMeal}</h3>
+          <ul >
+            <li>${mealsIngredients.strIngredient1}</li>
+            <li>${mealsIngredients.strIngredient2}</li>
+            <li>${mealsIngredients.strIngredient3}</li>
+            <li>${mealsIngredients.strIngredient4}</li>
+            <li>${mealsIngredients.strIngredient5}</li>
+            <li>${mealsIngredients.strIngredient6}</li>
+            <li>${mealsIngredients.strIngredient7}</li>
+            <li>${mealsIngredients.strIngredient8}</li>
+            <li>${mealsIngredients.strIngredient9}</li>
+            <li>${mealsIngredients.strIngredient10}</li>
+          </ul>
+         
+          <h4>Instructions details</h4>
+          <p>${mealsIngredients.strInstructions}</p>
+    </div>
+    
+    `;
+  };
